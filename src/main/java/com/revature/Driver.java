2019f6045583857ddd88e2e251067cea7e082a62
JJ Mubang <jmubang@gmail.com>
2018-06-20 13:54:57 -0400

    commented out cloud dependency

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -2,10 +2,9 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class Driver {
 	
 	public static void main(String[] args) {
