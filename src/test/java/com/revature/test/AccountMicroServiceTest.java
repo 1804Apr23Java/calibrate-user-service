0b6a8486880d87c4a9f5f965683ba3894fd3458d
JJ Mubang <jmubang@gmail.com>
2018-06-21 10:09:42 -0400

    added junit to pom and took out external library

--- src/test/java/com/revature/test/AccountMicroServiceTest.java
+++ src/test/java/com/revature/test/AccountMicroServiceTest.java
@@ -4,18 +4,32 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.revature.service.AccountService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Sql("classpath:populatedb.sql")
 public class AccountMicroServiceTest {
 	
-	
+	@Autowired
+	AccountService as;
 	
 	@Test 
-	public void firstPlayerScoreTest() {
+	public void getOneAccountTest() {
+//		assertEquals();
+	}
+	
+	@Test 
+	public void getAllAccountsTest() {
+//		assertEquals();
+	}
+	
+	@Test 
+	public void addAccountTest() {
 //		assertEquals();
 	}
 }
