c3b10a896fed03cae0214bfc0b92942ed9abe108
JJ Mubang <jmubang@gmail.com>
2018-06-25 01:07:13 -0400

    implemented logging with spring AOP, completed rest-assured test

--- src/main/java/com/revature/controller/AccountController.java
+++ src/main/java/com/revature/controller/AccountController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +25,6 @@ import com.revature.util.LoginData;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponses;
 
 @CrossOrigin
 @RestController
